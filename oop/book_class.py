a0d38e684b66cba7ef11cffe159207badfdad63a
Youssef-Nasr-Mansour <youssefnasr852@gmail.com>
2024-10-11 19:11:51 +0300

    Dive into Python Magic Methods

---
+++ oop/book_class.py
@@ -0,0 +1,35 @@
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        """Initialize a Book instance with title, author, and year."""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """Destructor that prints a message when a Book instance is deleted."""
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """Return a string representation of the Book instance."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Return an official string representation of the Book instance."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+from book_class import Book
+
+def main():
+    # Creating an instance of Book
+    my_book = Book("1984", "George Orwell", 1949)
+
+    # Demonstrating the __str__ method
+    print(my_book)  # Expected to use __str__
+
+    # Demonstrating the __repr__ method
+    print(repr(my_book))  # Expected to use __repr__
+
+    # Deleting a book instance to trigger __del__
+    del my_book
+
+if __name__ == "__main__":
+    main()
